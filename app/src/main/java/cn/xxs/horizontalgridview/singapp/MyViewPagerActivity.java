2a7cb15b66c0dfdd084932fd94e569d20e1b5a7f
谢文良 <765947965@qq.com>
2016-10-25 14:54:43 +0800

    11111

--- app/src/main/java/cn/xxs/horizontalgridview/singapp/MyViewPagerActivity.java
+++ app/src/main/java/cn/xxs/horizontalgridview/singapp/MyViewPagerActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.lzyzsd.randomcolor.RandomColor;
@@ -26,7 +27,7 @@ public class MyViewPagerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.viewpager);
-        TestBaseViewPager viewPager = (TestBaseViewPager) findViewById(R.id.viewPager);
+        final TestBaseViewPager viewPager = (TestBaseViewPager) findViewById(R.id.viewPager);
         ArrayList<String> arrayList = new ArrayList<>();
         final ArrayList<Integer> colors = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
@@ -39,6 +40,26 @@ public class MyViewPagerActivity extends Activity {
             protected void convert(ViewHolder holder, String item, List<String> list, int position) {
                 holder.getView(R.id.tvContent).setBackgroundColor(colors.get(position));
                 holder.setText(R.id.tvContent, item);
+            }
+        });
+        findViewById(R.id.btDown).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = viewPager.getCurrentItem() + 1;
+                if (current >= viewPager.getAdapter().getCount()) {
+                    current = 0;
+                }
+                viewPager.setCurrentItem(current);
+            }
+        });
+        findViewById(R.id.btUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = viewPager.getCurrentItem() - 1;
+                if (current < 0) {
+                    current = viewPager.getAdapter().getCount() - 1;
+                }
+                viewPager.setCurrentItem(current);
             }
         });
     }
